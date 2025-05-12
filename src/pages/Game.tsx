0125316db0f20a5aaf5aab5e1613e195ac3c39e4
baophan784 <70734934+baophan784@users.noreply.github.com>
2025-05-12 18:00:45 +0700

    Update Game.tsx

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -49,15 +49,15 @@ const Game = () => {
   // Tạo xác suất có trọng số
   const weightedRandom = () => {
     const rand = Math.random();
-    if (rand < 0.8) {
+    if (rand < 0.7) {
       // 60% xác suất: từ 1.0 đến 1.5
       return getRandomFloat(1.0, 1.5, 2);
     } else if (rand < 0.95) {
       // 25% xác suất: từ 1.5 đến 5
-      return getRandomFloat(1.5, 5.0, 2);
+      return getRandomFloat(1.5, 4.0, 2);
     } else {
       // 15% xác suất: từ 5 đến 20.99
-      return getRandomFloat(5.0, 20.99, 2);
+      return getRandomFloat(4.0, 20.99, 2);
     }
   };
 
